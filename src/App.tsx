6ba36bc99531eb5f8be32339ab575275ddd9c9c4
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-11-28 12:14:43 +0300

    added function toglestatus

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function App() {
     let [tasks, setTasks] = React.useState<TaskProps[]>([
         {id: 1, title: 'Task 1', isDone: false},
         {id: 2, title: 'Task 2', isDone: true},
-        {id: 3, title: 'Task 2', isDone: false},
-        {id: 4, title: 'Task 3', isDone: false},
+        {id: 3, title: 'Task 3', isDone: false},
+        {id: 4, title: 'Task 4', isDone: false},
     ]);
 
     let [title, setTitle] = useState<string>('');
@@ -25,37 +25,51 @@ function App() {
     }
 
     const OnChangeHundler = (e: ChangeEvent<HTMLInputElement>) => {
+
         setTitle(e.currentTarget.value);
     }
 
     const AddTask = () => {
+        if (title.trim() !== '') {
+            let NewTask: TaskProps = {
+                id: tasks.length + 1,
+                title: title,
+                isDone: false,
 
-        let NewTask: TaskProps = {
-            id: tasks.length + 1,
-            title: title,
-            isDone: false,
+            };
+            setTasks([NewTask, ...tasks])
+            setTitle('')
+        }
 
-        };
-        setTasks([NewTask, ...tasks])
-        setTitle('')
     }
 
+    const TogleStatus =(id: number) => {
+        setTasks(tasks.map(task => task.id === id ?
+            {...task, isDone: !task.isDone} : task
+        ))
 
-  return (
-    <div className="App">
-      <h2>What to learn</h2>
-        <input type="text" value={title} onChange={OnChangeHundler}/> <button onClick={AddTask}>+</button>
-        <ul>
-            {tasks.map((task) =>
-            <li key={task.id}>
-                <input type={'checkbox'} checked={task.isDone}></input>
-                <p>{task.title}</p>
-                <button onClick={()=>RemoveTask(task.id)}>remove</button>
-            </li>)}
 
-        </ul>
-    </div>
-  );
+        };
+
+
+
+
+    return (
+        <div className="App">
+            <h2>What to learn</h2>
+            <input type="text" value={title} onChange={OnChangeHundler}/>
+            <button onClick={AddTask}>+</button>
+            <ul>
+                {tasks.map((task) =>
+                    <li key={task.id}>
+                        <input type={'checkbox'} checked={task.isDone} onChange={()=>TogleStatus(task.id)}></input>
+                        <p>{task.title}</p>
+                        <button onClick={() => RemoveTask(task.id)}>remove</button>
+                    </li>)}
+
+            </ul>
+        </div>
+    );
 }
 
 export default App;
