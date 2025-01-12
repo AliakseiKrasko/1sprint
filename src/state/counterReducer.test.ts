ff6e13c655a88378060ac746cb90fec757ea3608
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-01-12 16:51:08 +0100

    added test reset_counter and increment_counter

--- src/state/counterReducer.test.ts
+++ src/state/counterReducer.test.ts
@@ -1,7 +1,17 @@
 import {counterReducer} from './counterReducer';
 
-test('INCREASING increments counter if below maxValue', () => {
-    const startState = {counter: 0};
-    const endState = counterReducer(startState, {type: 'INCREASING', maxValue: 10})
+test('increments counter if below maxValue', () => {
+    const startState = {counter: 0, maxValue: 10, mode: false};
+    const endState = counterReducer(startState, {type: 'INCREASING_COUNTER', maxValue: 10})
     expect(endState.counter).toBe(1)
+    expect(endState.maxValue).toBe(10)
+    expect(endState.mode).toBe(false)
+});
+
+test('reset counter', () => {
+    const startState = {counter: 5, maxValue: 10, mode: false};
+    const endState = counterReducer(startState,  {type: 'RESET_COUNTER', maxValue: 10})
+    expect(endState.counter).toBe(0)
+    expect(endState.maxValue).toBe(10)
+    expect(endState.mode).toBe(false)
 });
--- src/state/counterReducer.ts
+++ src/state/counterReducer.ts
@@ -1,27 +1,52 @@
+
 type StateType = {
-    counter: number
+    counter: number,
+    maxValue: number,
+    mode: boolean,
+}
+export const CounterState: StateType = {
+    counter: 0,
+    maxValue: 10,
+    mode: false,
 }
 
 export type IncrimentCounter = {
-    type: 'INCREASING',
-
+    type: 'INCREASING_COUNTER',
     maxValue: number
 }
 
-export const CounterState: StateType = {
-    counter: 0,
+export type ResetCounter = {
+    type: 'RESET_COUNTER',
+    maxValue: number,
+
 }
 
-type ActionType = IncrimentCounter;
+export type SaveValuesCounter = {
+    type: 'SAVE_VALUE',
+    value: number,
+    maxValue: number
+}
+
+type ActionType = IncrimentCounter | ResetCounter | SaveValuesCounter;
 
 
 export const counterReducer = (state: StateType = CounterState, action: ActionType) => {
     switch (action.type) {
-        case 'INCREASING':
+        case 'INCREASING_COUNTER':
             return {
                 ...state,
-                counter: state.counter < action.maxValue ?  state.counter + 1 : state.counter,
+                counter: state.counter < action.maxValue ? state.counter + 1 : state.counter,
             }
+        case 'RESET_COUNTER':
+            return {
+                ...state,
+                counter: 0, maxValue: 10
+            }
+            case 'SAVE_VALUE':
+                return {
+                    ...state,
+                    counter: action.value, maxValue: action.maxValue
+                }
 
         default:
             return state;
